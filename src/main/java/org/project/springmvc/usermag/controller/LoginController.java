0d20f726a7a1ba0698db2b12db0136da6d52c5e9
dongronglong <24197396@qq.com>
2017-12-25 17:28:22 +0800

    更新了代码

--- src/main/java/org/project/springmvc/usermag/controller/LoginController.java
+++ src/main/java/org/project/springmvc/usermag/controller/LoginController.java
@@ -3,6 +3,7 @@ package org.project.springmvc.usermag.controller;
 import org.project.springmvc.beans.UserBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -39,7 +40,18 @@ public class LoginController {
         System.out.println(user);
         return null;
     }
-    @RequestMapping("/login2")
+
+    /**
+     * value = {"/login2"}  完成路径映射，表示/login3这种路径发出请求，由以下方法执行
+     * value={"/login3","/login4"},表示/login3、/login这两个路径发出请求，由以下方法执行
+     * value={"/login*"},表示可以匹配/login123、/login、/loginasdsa
+     * value={"/login/*},表示可以匹配/login/asd
+     * value={"/login?"},表示可以匹配/logins、/login2
+     * value={"/login**"},表示可以匹配/login/asd/asddx.
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = {"/login2"},method = {RequestMethod.POST})
     public ModelAndView login(UserBean user){
         System.out.println(user);
         return null;
